33c984cc19a115c9d2af6bdae368aecf56b5c14f
Dan Rathbone <daniel.rathbone@bskyb.com>
2013-06-05 15:10:01 +0100

    Vaguely working tarball builder

---
+++ js/test1.js
@@ -0,0 +1,10 @@
+/**
+ * Some comments that shouldnt end up in the build
+ */
+function helloWorld() {
+    alert("hello world");
+    var wibble1 = "blah blah";
+    var wibble2 = "stUFF";
+    var wibble3 = wibble2.toLowerCase() + wibble1;
+    alert(wibble3);
+}
